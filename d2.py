2bf2d3bab746ddb77dfe7cf25fc5b1149bcaf02b
Will Dufault <99445180+willdufault@users.noreply.github.com>
2024-12-02 09:38:21 -0500

    Create d2.py

---
+++ d2.py
@@ -0,0 +1,40 @@
+data = """\
+"""
+
+reports = []
+for line in data.split("\n"):
+    report = []
+    for num in line.split():
+        report.append(int(num))
+    reports.append(report)
+
+# Part 1.
+safe_cnt = 0
+for report in reports:
+    inc = report[1] > report[0]
+    prv = report[0]
+    for lvl in report[1:]:
+        diff = abs(lvl - prv)
+        if diff < 1 or diff > 3 or (lvl > prv) != inc:
+            break
+        prv = lvl
+    else:
+        safe_cnt += 1
+print(safe_cnt)
+
+# Part 2.
+safe_cnt = 0
+for report in reports:
+    for idx in range(len(report)):
+        damp = report[:idx] + report[idx + 1 :]
+        inc = damp[1] > damp[0]
+        prv = damp[0]
+        for lvl in damp[1:]:
+            diff = abs(lvl - prv)
+            if diff < 1 or diff > 3 or (lvl > prv) != inc:
+                break
+            prv = lvl
+        else:
+            safe_cnt += 1
+            break
+print(safe_cnt)
